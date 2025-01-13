78cb5c35b110e97aa0e95635b3bfd408a632e271
xsergantx <sergey.yurga42@gmail.com>
2025-01-13 13:51:19 +0700

    Update module_13_2.py

--- Модуль 13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_2.py
+++ Модуль 13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_2.py
@@ -13,7 +13,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7582957828:AAG7NpEHnKHB8-QQj1mV7L7XwtAfCTXmTsc"
+api = ""
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
