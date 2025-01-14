cf7ed3fec6c97e3461d7d320da381eb9ea1cb83a
xsergantx <sergey.yurga42@gmail.com>
2025-01-14 11:41:48 +0700

    Update module_13_3.py

--- Модуль 13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_3.py
+++ Модуль 13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_3.py
@@ -15,13 +15,15 @@ bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
 
-@dp.message_handler(text = ["Привет"])
-async def all_message(message):
-    await message.answer("Введите команду /start, чтобы начать общение.")# то ответ будет такой в ТГ
+ # message_handler - обработчик входящих сообщений
+@dp.message_handler(commands = ["start"])                  # перехватывает определенные комады после "/"
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
-@dp.message_handler(text = ["/start"])
+@dp.message_handler()                                      # перехватывает все осташие сообщения
 async def all_message(message):
-    await message.answer("Привет! Я бот помогающий твоему здоровью")
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
