3835051ecfd256145abd7bba3acc00e917d22cf7
xsergantx <sergey.yurga42@gmail.com>
2025-01-15 13:44:09 +0700

    Add files via upload

---
+++ Модуль 13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_6.py
@@ -0,0 +1,150 @@
+"""
+Цель: научится создавать Inline клавиатуры и кнопки на них в Telegram-bot.
+
+Задача "Ещё больше выбора":
+Необходимо дополнить код предыдущей задачи, чтобы при нажатии на кнопку 'Рассчитать' присылалась Inline-клавиатруа.
+Создайте клавиатуру InlineKeyboardMarkup с 2 кнопками InlineKeyboardButton:
+С текстом 'Рассчитать норму калорий' и callback_data='calories'
+С текстом 'Формулы расчёта' и callback_data='formulas'
+Создайте новую функцию main_menu(message), которая:
+Будет обёрнута в декоратор message_handler, срабатывающий при передаче текста 'Рассчитать'.
+Сама функция будет присылать ранее созданное Inline меню и текст 'Выберите опцию:'
+Создайте новую функцию get_formulas(call), которая:
+Будет обёрнута в декоратор callback_query_handler, который будет реагировать на текст 'formulas'.
+Будет присылать сообщение с формулой Миффлина-Сан Жеора.
+Измените функцию set_age и декоратор для неё:
+Декоратор смените на callback_query_handler, который будет реагировать на текст 'calories'.
+Теперь функция принимает не message, а call. Доступ к сообщению будет следующим - call.message.
+По итогу получится следующий алгоритм:
+Вводится команда /start
+На эту команду присылается обычное меню: 'Рассчитать' и 'Информация'.
+В ответ на кнопку 'Рассчитать' присылается Inline меню: 'Рассчитать норму калорий' и 'Формулы расчёта'
+По Inline кнопке 'Формулы расчёта' присылается сообщение с формулой.
+По Inline кнопке 'Рассчитать норму калорий' начинает работать машина состояний по цепочке.
+"""
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+# Инициализация бота и диспетчера
+API_TOKEN = ''
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+# Определение группы состояний
+class UserState(StatesGroup):
+    gender = State()
+    age = State()
+    growth = State()
+    weight = State()
+
+
+# Функция для начала цепочки
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton('Рассчитать'), types.KeyboardButton('Информация'))
+    await message.answer('Выберите действие:', reply_markup=keyboard)
+
+
+# Функция для отправки Inline-клавиатуры
+@dp.message_handler(lambda message: message.text == 'Рассчитать')
+async def main_menu(message: types.Message):
+    inline_keyboard = types.InlineKeyboardMarkup()
+    inline_keyboard.add(types.InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories'))
+    inline_keyboard.add(types.InlineKeyboardButton('Формулы расчёта', callback_data='formulas'))
+    await message.answer('Выберите опцию:', reply_markup=inline_keyboard)
+
+
+# Функция для отправки информации о боте
+@dp.message_handler(lambda message: message.text == 'Информация')
+async def bot_info(message: types.Message):
+    info = (
+        "Этот бот помогает рассчитать вашу суточную норму калорий по формуле Миффлина-Сан Жеора.\n\n"
+        "Для расчета нормы калорий нажмите кнопку 'Рассчитать' и следуйте инструкциям.\n\n"
+        "Разработчик: AlexBush"
+    )
+    await message.answer(info)
+
+
+# Функция для отправки формул расчета калорий
+@dp.callback_query_handler(lambda call: call.data == 'formulas')
+async def get_formulas(call: types.CallbackQuery):
+    formulas = (
+        "Формула Миффлина-Сан Жеора для мужчин:\n"
+        "10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) + 5\n\n"
+        "Формула Миффлина-Сан Жеора для женщин:\n"
+        "10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) - 161"
+    )
+    await call.message.answer(formulas)
+    await call.answer()
+
+
+# Функция для начала цепочки расчета калорий
+@dp.callback_query_handler(lambda call: call.data == 'calories')
+async def set_gender(call: types.CallbackQuery):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Мужской", "Женский")
+    await call.message.answer('Выберите пол:', reply_markup=keyboard)
+    await UserState.gender.set()
+
+
+# Функция для обработки пола
+@dp.message_handler(state=UserState.gender)
+async def set_age(message: types.Message, state: FSMContext):
+    gender = message.text
+    await state.update_data(gender=gender)
+    await message.answer('Введите свой возраст:', reply_markup=types.ReplyKeyboardRemove())
+    await UserState.age.set()
+
+
+# Функция для обработки возраста
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+# Функция для обработки роста
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+# Функция для обработки веса и вычисления нормы калорий
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+    gender = data['gender']
+
+    if gender == "Мужской":
+        # Формула Миффлина - Сан Жеора для мужчин
+        calories = 10 * weight + 6.25 * growth - 5 * age + 5
+    elif gender == "Женский":
+        # Формула Миффлина - Сан Жеора для женщин
+        calories = 10 * weight + 6.25 * growth - 5 * age - 161
+    else:
+        await message.answer("Неверный пол. Пожалуйста, начните снова.")
+        await state.finish()
+        return
+
+    await message.answer(f'Ваша норма калорий: {calories:.2f} ккал в день.')
+    await state.finish()
+
+
+# Запуск бота
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
